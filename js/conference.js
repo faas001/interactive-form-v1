dc40bfd13c87a192a6f007ff703df4355e59585e
faas001 <42976743+faas001@users.noreply.github.com>
2018-10-05 23:32:32 -0400

    1.3
    Working prototype of meets expectations

--- js/conference.js
+++ js/conference.js
@@ -9,11 +9,39 @@ function initPage() {
     $('.activities').append('<label>Total cost for selected activities: <strong>$<span id="displayCost"></span></strong></label>');
     $('.activities').append('<label id="test"></label>');
     $('#displayCost').html(totalCost);
+    $('#payment').val('credit card');
+    $('#payment option[value="select_method"]').remove();
+    $('#credit-card').next().attr('id','paypal').hide();
+    $('#paypal').next().attr('id','bitcoin').hide();
+  
 }
 
+function clearErrors() {
+    $('#nameError').remove();
+    $('#name').css('border-color', '#c1deeb');
+    $('#mailError').remove();
+    $('#mail').css('border-color', '#c1deeb');
+    $('#activityError').remove();
+    $('.activities').css({'border-right':'none','border-color':'red'});
+    $('#ccError').remove();
+    $('#cc-num').css('border-color', '#c1deeb');
+    $('#zipError').remove();
+    $('#zip').css('border-color', '#c1deeb');
+    $('#cvvError').remove();
+    $('#cvv').css('border-color', '#c1deeb');
+}
 
-$('.container').on('click', (e) => {
+$('.container').on('click change keypress', (e) => {
     
+    if(e.target === $('#name')[0]) {
+        $('#nameError').remove();
+        $('#name').css('border-color', '#c1deeb');
+    }
+    if(e.target === $('#mail')[0]) {
+        $('#mailError').remove();
+        $('#mail').css('border-color', '#c1deeb');
+    }    
+        
         if(e.target === $('#title')[0]) {
             if ($('select#title').val() === 'other') {
                 $('#other-title').show();
@@ -57,15 +85,33 @@ $('.container').on('click', (e) => {
                 $('#colors-js-puns').hide();
                        
              }
-        } 
+        }
+        console.log(e.target);
+        if(e.target === $('#cc-num')[0]) {
+            
+            $('#ccError').remove();
+            $('#cc-num').css('border-color', '#c1deeb');
+        }
+        if(e.target === $('#zip')[0]) {
+          
+            $('#zipError').remove();
+            $('#zip').css('border-color', '#c1deeb');
+        }
+        if(e.target === $('#cvv')[0]) {
+          
+            $('#cvvError').remove();
+            $('#cvv').css('border-color', '#c1deeb');
+        }
+    
 });
 
-$('.activities').on('click', (e) => {
+$('.activities').on('click change', (e) => {
     const act = $(e.target).parent().parent();
     activity = $(act)[0] === $('.activities')[0];
     
         if(activity) {
-           
+            $('#activityError').remove();
+            $('.activities').css({'border-right':'none','border-color':'red'});
             
             console.log('Activities clicked');
             console.log($(e.target).is(':checked'));
@@ -117,10 +163,91 @@ $('.activities').on('click', (e) => {
                     $('input[name="js-libs"]').parent().css('color', 'black');
                }
             }
-          
+                   
+        }
+});       
+
+    $('#payment').on('change', (e) => {
+        if (e.target === $('#payment')[0]) {
+            console.log('payment processing');
+            console.log($(e.target).val());
+
+            if ($(e.target).val() === 'bitcoin') {
+                $('#bitcoin').show();
+                $('#credit-card').hide();
+                $('#paypal').hide();
+            } else if ($(e.target).val() === 'paypal') {
+                $('#bitcoin').hide();
+                $('#credit-card').hide();
+                $('#paypal').show();
+            } else {
+                $('#bitcoin').hide();
+                $('#credit-card').show();
+                $('#paypal').hide();
+            }
             
         }
-});
+       
+    });
+
+    //straight up copy/pasted this from Stackoverflow top voted answer on email validation from user: rnevius
+    function validateEmail(email) {
+        var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        return re.test(String(email).toLowerCase());
+    }
+
+    $('button').on('click', (e) => {
+        clearErrors();
+        if(! $('#name').val()) {
+        e.preventDefault();
+        alert('Name field is blank');
+        $('#name').prev().append('<span id="nameError" style="color:yellow"> Please enter a name so that we know who to register!</span>');
+        $('#name').css('border-color', 'red');
+        console.log(e);
+        } 
+
+        if(! validateEmail($('#mail').val())) {
+            e.preventDefault();
+            alert('Email field is not valid');
+            $('#mail').prev().append('<span id="mailError" style="color:yellow"> Please enter a valid email address.</span>');
+            $('#mail').css('border-color', 'red');
+            console.log(e);
+            }
+
+        if ($('input[type="checkbox"]').is(':checked')) { 
+            e.preventDefault();
+            alert('At least one activity checked');
+          
+        } else { 
+            console.log('No activity selected');
+            $('.activities').children('legend').append('<span id="activityError" style="color:yellow"> Select at least one option. No free rides! =)</span>');
+            $('.activities').css({'border-right':'2px solid','border-color':'red'});
+            }
+
+            console.log($('#cc-num').val())
+        if( $('#payment').val() === 'credit card') {
+            if( $('#cc-num').val() === '') {
+                e.preventDefault();
+                alert('No cc number entered');
+                $('#cc-num').prev().append('<span id="ccError" style="color:yellow"> Between 13-16 numbers.</span>');
+                $('#cc-num').css('border-color', 'red');
+            }
+            if( $('#zip').val() === '') {
+                e.preventDefault();
+                alert('no zip code entered');
+                $('#zip').prev().append('<span id="zipError" style="color:yellow"> Required</span>');
+                $('#zip').css('border-color', 'red');
+            }
+            if( $('#cvv').val() === '') {
+                e.preventDefault();
+                alert('no cvv entered');
+                $('#cvv').prev().append('<span id="cvvError" style="color:yellow"> 3-digit only</span>');
+                $('#cvv').css('border-color', 'red');
+            }
+        }
+
+    });
+
 
 
 
